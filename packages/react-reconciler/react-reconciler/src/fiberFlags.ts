3e2dc79e10a0665eb10d4f990f8e384b2a532ae7
kyoonart <1564328778@qq.com>
2024-01-13 15:28:37 +0800

    feat: delete

--- packages/react-reconciler/react-reconciler/src/fiberFlags.ts
+++
@@ -1,8 +0,0 @@
-export type Flags = number;
-
-export const NoFlags = 0b000000000000000000000000000;
-export const Placement = 0b000000000000000000000000010;
-export const Update = 0b000000000000000000000000100;
-export const ChildDeletion = 0b000000000000000000000010000;
-
-export const MutationMask = Placement | Update | ChildDeletion;
